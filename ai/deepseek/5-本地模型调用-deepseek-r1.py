516b0589f970f7ece173b0aa2a981396c8fb5f96
huangrx <huang.rx.life@hotmail.com>
2025-02-15 18:11:50 +0800

    feat: AI Learning

--- ai/deepseek/5-本地模型调用-deepseek-r1.py
+++ ai/deepseek/5-本地模型调用-deepseek-r1.py
@@ -1,7 +1,12 @@
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core.llms import ChatMessage
 
-llm = HuggingFaceLLM(model='/Users/hrenxiang/.ollama/models/manifests/registry.ollama.ai/library/deepseek-r1')
+llm = HuggingFaceLLM(
+    model_name='/Users/hrenxiang/Documents/company/ai/deepseek/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B',
+    tokenizer='/Users/hrenxiang/Documents/company/ai/deepseek/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B',
+    model_kwargs={'trust_remote_code': True},
+    tokenizer_kwargs={')trust_remote_code': True},
+)
 
 resp = llm.chat(messages=[ChatMessage(content='你好！')])
 
